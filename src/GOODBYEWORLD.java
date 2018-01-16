e03aaa50871b35661babdced07c999d998ad124c
SHiFTiNG_CORE <SHiFTiNG_CORE@iSC_PC>
2018-01-16 23:34:41 +0800

    First Commit

---
+++ src/GOODBYEWORLD.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author SHiFTiNG_CORE
+ */
+public class GOODBYEWORLD {
+    public static void main(String[] args) {
+        System.out.println("GOODBYE BS WORLD!!!");
+    }
+}
